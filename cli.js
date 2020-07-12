4c25361752ff74a2bbe0aff779c250f0a7c08029
fabioluzm <fabioluzm@gmail.com>
2020-07-12 17:12:11 +0100

    master fixes

--- cli.js
+++ cli.js
@@ -2,7 +2,9 @@
 const inquirer = require('inquirer');
 const colors = require('colors');
 const fs = require('fs');
+const { exec, execSync } = require('child_process');
 
+// read templates from package template folder
 const CHOICES = fs.readdirSync(`${__dirname}/templates`);
 
 const QUESTIONS = [
@@ -26,7 +28,9 @@ const QUESTIONS = [
 // grab arguments from cli to be used as project name
 const argument = process.argv[2];
 const checkQuestions = (argument === undefined || argument === "") ? QUESTIONS : QUESTIONS[0];
+// const projectName = (checkQuestions === QUESTIONS[0]) ? argument : answers['project-name'];
 
+// current prompt directory
 const CURR_DIR = process.cwd();
 
 inquirer.prompt(checkQuestions)
@@ -34,12 +38,25 @@ inquirer.prompt(checkQuestions)
     const projectChoice = answers['project-choice'];
     const projectName = (checkQuestions === QUESTIONS[0]) ? argument : answers['project-name'];
     const templatePath = `${__dirname}/templates/${projectChoice}`;
-  
+    
+    // create template directory
     fs.mkdirSync(`${CURR_DIR}/${projectName}`);
-
+    
+    // create template content
+    console.log('\nGenerating project...'.yellow.bold);
     createDirectoryContents(templatePath, projectName);
 
-    console.log(`\n${projectName} created successfully.`.green.bold);
+    // check if package.json exists
+    if (fs.existsSync(`${templatePath}/package.json`)) {
+      installDependencies(projectName);
+    }
+
+    installGit(projectName);
+    
+    console.log('Done.'.cyan.bold);
+  })
+  .catch(err => {
+    console.log(err);
   });
 
 function createDirectoryContents (templatePath, newProjectPath) {
@@ -59,11 +76,38 @@ function createDirectoryContents (templatePath, newProjectPath) {
 
       const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
       fs.writeFileSync(writePath, contents, 'utf8');
+
     } else if (stats.isDirectory()) {
       fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
       
       // recursive call
       createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
     }
+  });
+}
+
+const installDependencies = (projectName) => {
+  execSync(`cd ${projectName} && npm install --silent`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`.red.bold);
+      return;
+    }
+    // console.log(`stdout: ${stdout}`);
+    // console.error(`stderr: ${stderr}`);
+  });
+}
+
+const installGit = (projectName) => {
+  const gitInit = 'git init';
+  const gitAdd = 'git add .';
+  const gitCommit = 'git commit -m "Initialize project using Create App Template"';
+
+  execSync(`cd ${projectName} && ${gitInit} && ${gitAdd} && ${gitCommit}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    // console.log(`stdout: ${stdout}`);
+    // console.error(`stderr: ${stderr}`);
   });
 }
